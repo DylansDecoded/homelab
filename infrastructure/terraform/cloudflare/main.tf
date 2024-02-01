c3193c770226e66df1e6bde49efbb312eb89c0e2
Dylan Robson <dylan@robsonhome.cloud>
2024-01-31 22:33:11 -0500

    Add new files and roles for Docker and Ansible configurations

---
+++ infrastructure/terraform/cloudflare/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  cloud {
+    organization = "DylansDecoded"
+
+    workspaces {
+      name = "arpa-home-cloudflare"
+    }
+  }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "4.23.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.4.1"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
+module "onepassword_item" {
+  source = "github.com/bjw-s/terraform-1password-item?ref=main"
+  vault  = "kubernetes"
+  item   = "cloudflare"
+}
+
+data "http" "ipv4_lookup_raw" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+data "cloudflare_zone" "domain" {
+  name = "robsonhome.cloud"
+}
