a433934b1af41b34297169acbe138eb6d952c1c8
Dylan Robson <dylan@robsonhome.cloud>
2024-03-19 10:49:55 -0400

    Add additional torznab sources to config.js

--- kubernetes/main/apps/default/cross-seed/app/resources/config.js
+++ kubernetes/main/apps/default/cross-seed/app/resources/config.js
@@ -5,7 +5,11 @@ module.exports = {
   torznab: [
     `http://prowlarr.default.svc.cluster.local:9696/1/api?apikey=$${process.env.PROWLARR_API_KEY}`, // fnp
     `http://prowlarr.default.svc.cluster.local:9696/2/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ipt
-    `http://prowlarr.default.svc.cluster.local:9696/3/api?apikey=$${process.env.PROWLARR_API_KEY}` // milk
+    `http://prowlarr.default.svc.cluster.local:9696/3/api?apikey=$${process.env.PROWLARR_API_KEY}`, // milk
+    `http://prowlarr.default.svc.cluster.local:9696/4/api?apikey=$${process.env.PROWLARR_API_KEY}`, //1337x
+    `http://prowlarr.default.svc.cluster.local:9696/5/api?apikey=$${process.env.PROWLARR_API_KEY}`, // kbn
+    `http://prowlarr.default.svc.cluster.local:9696/6/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ia
+    `http://prowlarr.default.svc.cluster.local:9696/7/api?apikey=$${process.env.PROWLARR_API_KEY}` //rarbg
   ],
   port: process.env.CROSSSEED_PORT || 80,
   apiAuth: false,
