933c50fcf0c10439fd0fb12d01b0653d95871e9c
Dylan Robson <dylan@robsonhome.cloud>
2024-03-19 09:47:06 -0400

    Update webhook URL in completed.sh script

--- kubernetes/main/apps/default/qbittorrent/app/resources/completed.sh
+++ kubernetes/main/apps/default/qbittorrent/app/resources/completed.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # qBittorrent settings > 'Run external program on torrent finished'
 # /scripts/completed.sh "%F"
-/usr/bin/curl --silent --request POST --data-urlencode "path=$1" http://cross-seed.downloads.svc.cluster.local:2468/api/webhook
+/usr/bin/curl --silent --request POST --data-urlencode "path=$1" http://cross-seed.default.svc.cluster.local/api/webhook
