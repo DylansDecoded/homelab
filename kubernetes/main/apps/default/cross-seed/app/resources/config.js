3e14386708571d871b20581b4b59b6d9b91291d3
Dylan <dylan@robsonhome.cloud>
2024-05-14 13:57:24 -0400

    chore: Update cross-seed torznab URLs to use port 9696

--- kubernetes/main/apps/default/cross-seed/app/resources/config.js
+++ kubernetes/main/apps/default/cross-seed/app/resources/config.js
@@ -3,15 +3,15 @@ module.exports = {
   delay: 20,
   qbittorrentUrl: "http://qbittorrent.default.svc.cluster.local:8080",
   torznab: [
-    `http://prowlarr.default.svc.cluster.local/3/api?apikey=$${process.env.PROWLARR_API_KEY}`, // mam
-    `http://prowlarr.default.svc.cluster.local/5/api?apikey=$${process.env.PROWLARR_API_KEY}`, // hds
-    `http://prowlarr.default.svc.cluster.local/9/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ipt
-    `http://prowlarr.default.svc.cluster.local/11/api?apikey=$${process.env.PROWLARR_API_KEY}`, // dc
-    `http://prowlarr.default.svc.cluster.local/14/api?apikey=$${process.env.PROWLARR_API_KEY}`, // thhq
-    `http://prowlarr.default.svc.cluster.local/16/api?apikey=$${process.env.PROWLARR_API_KEY}`, // tos
-    `http://prowlarr.default.svc.cluster.local/17/api?apikey=$${process.env.PROWLARR_API_KEY}`, // mlk
-    `http://prowlarr.default.svc.cluster.local/53/api?apikey=$${process.env.PROWLARR_API_KEY}`, // fnp
-    `http://prowlarr.default.svc.cluster.local/86/api?apikey=$${process.env.PROWLARR_API_KEY}`, // uplcx
+    `http://prowlarr.default.svc.cluster.local:9696/3/api?apikey=$${process.env.PROWLARR_API_KEY}`, // mam
+    `http://prowlarr.default.svc.cluster.local:9696/5/api?apikey=$${process.env.PROWLARR_API_KEY}`, // hds
+    `http://prowlarr.default.svc.cluster.local:9696/9/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ipt
+    `http://prowlarr.default.svc.cluster.local:9696/11/api?apikey=$${process.env.PROWLARR_API_KEY}`, // dc
+    `http://prowlarr.default.svc.cluster.local:9696/14/api?apikey=$${process.env.PROWLARR_API_KEY}`, // thhq
+    `http://prowlarr.default.svc.cluster.local:9696/16/api?apikey=$${process.env.PROWLARR_API_KEY}`, // tos
+    `http://prowlarr.default.svc.cluster.local:9696/17/api?apikey=$${process.env.PROWLARR_API_KEY}`, // mlk
+    `http://prowlarr.default.svc.cluster.local:9696/53/api?apikey=$${process.env.PROWLARR_API_KEY}`, // fnp
+    `http://prowlarr.default.svc.cluster.local:9696/86/api?apikey=$${process.env.PROWLARR_API_KEY}`, // uplcx
   ],
   port: process.env.CROSSSEED_PORT || 80,
   apiAuth: false,
